9367f7413e54f08bc1f0ffac85b6a7534f326a39
Rishanthakumar <rishanthakumar@gmail.com>
2017-11-28 19:04:53 +0530

    Add urls

--- app/config.js
+++ app/config.js
@@ -1,6 +1,11 @@
 
+// var iframeHref = "http://35.198.251.53";
+// var host = 'http://35.198.251.53:4567/';
+// var nlu = 'http://35.198.251.53:4568/';
+// var sock = 'http://35.198.251.53:3000';
+
 var iframeHref = "http://35.198.251.53";
-var host = 'http://35.198.251.53:4567/';
-var nlu = 'http://35.198.251.53:4568/';
+var host = 'http://localhost:4567/';
+var nlu = 'http://localhost:4568/';
 var sock = 'http://35.198.251.53:3000';
  
